c37a107924ba21bfac3c60fce4ed8363fa227c59
npcat <beijimiao@qq.com>
2025-11-11 10:31:38 +0000

    remove unused files

--- xadmin_auth/exception.py
+++
@@ -1,9 +0,0 @@
-from http import HTTPStatus
-from django.http import Http404
-
-
-class HttpNotFound(Http404):
-    def __init__(self, msg='the requested resource is not found.'):
-        self.status = HTTPStatus.NOT_FOUND
-        self.msg = msg
-        super().__init__(self.msg)
